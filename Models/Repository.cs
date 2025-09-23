562dbc91f09fea83bb57740321aa2629ed290d50
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-09-24 01:59:47 +0300

    first commit

---
+++ Models/Repository.cs
@@ -0,0 +1,44 @@
+//veritabanı yerine kullanacağımız kısım 
+
+namespace FormsApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _products = new(); //static yaparak bellekte tek bir kopya oluşturduk. Product sınıfından nesnelerden oluşan liste oluşturduk. 
+        
+        private static readonly List<Category> _categories = new();
+
+        static Repository()
+        {
+            _categories.Add(new Category { CategoryId = 1, Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Bilgisayar" });
+
+            _products.Add(new Product { ProductId = 1,Name = "iphone 11", Price=40000, IsActive=true, Image="1.jpg",   CategoryId = 1 });
+            _products.Add(new Product { ProductId = 2,Name = "iphone 14", Price=50000, IsActive=true, Image="2.jpg",   CategoryId = 1 });
+            _products.Add(new Product { ProductId = 3,Name = "iphone 15", Price=60000, IsActive=true, Image="3.jpg",   CategoryId = 1 });
+            _products.Add(new Product { ProductId = 4,Name = "iphone 16", Price=70000, IsActive=true, Image="4.jpg",   CategoryId = 1 });
+            _products.Add(new Product { ProductId = 5,Name = "iphone 17", Price=80000, IsActive=true, Image="5.jpg",   CategoryId = 1 });
+            _products.Add(new Product { ProductId = 6,Name = "Macbook Air",Price=90000,IsActive=true, Image="6.jpg", CategoryId = 2 });
+            _products.Add(new Product { ProductId = 7,Name = "Mabook Pro ",Price=100000,IsActive=true, Image="7.jpg", CategoryId = 2 });
+          
+        }
+
+        public static List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+
+        public static List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+        }
+
+
+    }
+}
